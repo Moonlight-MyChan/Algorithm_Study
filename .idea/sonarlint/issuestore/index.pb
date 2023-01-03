cd4271f9b29209bbb4c1622ebaa8d9d14b63a502
Moonlight-MyChan <chan306@naver.com>
2023-01-03 21:39:27 +0900

    commit

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,11 +1,3 @@
 
-N
-src/Inflearn/Inflearn_2_6.java,c\9\c973073994ff9bd4f12435ea39de31b7a075ebba
-N
-src/Inflearn/Inflearn_2_7.java,4\7\47652025a6227ac738f38b311774c572a07fb18b
-N
-src/Inflearn/Inflearn_2_9.java,7\2\72ee20e99fdfd70428837ef1ac0b0e091aac9a2e
-N
-src/Inflearn/Inflearn_2_8.java,b\d\bd9af942f161efdd4ef5807f42c81631c33a11b0
 O
 src/Inflearn/Inflearn_2_10.java,6\0\6085793633e3fe05e955be51ec7260003544ba41
--- Algorithm Study/src/Inflearn/Inflearn_2_11.java
+++ Algorithm Study/src/Inflearn/Inflearn_2_11.java
@@ -12,24 +12,48 @@ public class Inflearn_2_11 {
 
         int N  = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        int [][] arr = new int[N][N];
+        int [][] arr = new int[N + 1][6];
 
-        for (int i = 0; i < N; i++)
+        for (int i = 1; i <= N; i++)
         {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++)
+            for (int j = 1; j <= 5; j++)
             {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        System.out.println("Test");
+        System.out.println(Solution(arr, N));
 
 	}
 
-	// public static int Solution (int [][] arr, int N)
-	// {
-        
+	public static int Solution (int [][] arr ,int N)
+	{
+        int answer = 0;
+        int max = Integer.MIN_VALUE;
 
-    // }
+        for (int i = 1; i <= N; i++)
+        {
+            int cnt = 0;
+            for (int j = 1; j <= N; j++)
+            {
+                if(i==j) continue;
+                for (int k = 1; k <= 5; k++)
+                {
+                    if(arr[i][k] == arr[j][k])
+                    {
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+            if(cnt > max)
+            {
+                max = cnt;  
+                answer = i;
+            }
+        }
+
+        return answer;
+    }
 }
